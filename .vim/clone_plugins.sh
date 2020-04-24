c58ef80fec0ca9c4e053ddbf1ae2e49656a582c0
LongerHV <46924944+LongerHV@users.noreply.github.com>
2020-04-24 12:38:36 +0200

    Fixed clone plugins

--- .vim/clone_plugins.sh
+++ .vim/clone_plugins.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 mkdir ~/.vim/bundle
-cd ~/.vim
-git clone https://github.com/vim-scripts/AutoClose bundle/AutoClose
-git clone https://github.com/ctrlpvim/ctrlp.vim  bundle/ctrlp.vim
-git clone https://github.com/yggdroot/indentline bundle/indentline
-git clone https://github.com/scrooloose/nerdcommenter  bundle/nerdcommenter
-git clone https://github.com/scrooloose/nerdtree bundle/nerdtree
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin bundle/nerdtree-git-plugin
-git clone https://github.com/vim-airline/vim-airline bundle/vim-airline
-git clone https://github.com/tpope/vim-fugitive/ bundle/vim-fugitive
-git clone https://github.com/airblade/vim-gitgutter bundle/vim-gitgutter
-git clone https://github.com/jistr/vim-nerdtree-tabs.git bundle/vim-nerdtree-tabs
-git clone https://github.com/sheerun/vim-polyglot bundle/vim-polyglot
-git clone https://github.com/tpope/vim-repeat bundle/vim-repeat
-git clone https://github.com/tpope/vim-surround bundle/vim-surround
+cd ~/.vim/bundle
+git clone https://github.com/vim-scripts/AutoClose
+git clone https://github.com/ctrlpvim/ctrlp.vim
+git clone https://github.com/yggdroot/indentline
+git clone https://github.com/scrooloose/nerdcommenter
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+git clone https://github.com/sheerun/vim-polyglot
+git clone https://github.com/tpope/vim-repeat
+git clone https://github.com/tpope/vim-surround
 git clone https://github.com/valloric/youcompleteme
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/ryanoasis/vim-devicons
