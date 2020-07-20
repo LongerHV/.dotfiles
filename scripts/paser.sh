082a4b1d9f9dc35530c9ebf7a7bc22f2b1d4eb03
LongerHV <michal@mieszczak.com>
2020-07-20 17:48:43 +0200

    paser can be stopped, prints info if package is installed

--- scripts/paser.sh
+++ scripts/paser.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Interactive sarch and install pacman packages
+
 export FZF_DEFAULT_OPTS='
 --height=30%
 --layout=reverse
@@ -9,10 +11,12 @@ package=$(pacman -Ss | \
     awk 'NR % 2 == 1 { print $1 }' | \
     fzf)
 
-pacman -Si $package | less
-
 [ -z "$package" ] && exit 1
+pacman -Si $package | less
+package_short=$(echo $package | awk -F'/' '{print $2}')
+
 echo ":: Selected package: $package"
+pacman -Qq $package_short &> /dev/null && echo ":: Package already in the system"
 read -p ":: Install package? [y/N] " ans
 if [ "$ans" == "y" ]
 then
