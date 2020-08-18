49a303ef0bfac09bc7c59aa53e5d27971cae2ea2
LongerHV <michal@mieszczak.com>
2020-08-18 22:39:31 +0200

    refactored script

--- scripts/bing_image.sh
+++ scripts/bing_image.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# initial wallpaper set
 nitrogen --restore
 
+# setting paths
 images_dir="$HOME/.local/share/wallpapers"
-
-url_preffix="http://www.bing.com"
-url_suffix=$(curl --no-progress-meter "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1" | xmllint --xpath "string(//url)" -) || exit 1
-
-url="$url_preffix$url_suffix"
-name=$(echo $url | awk -F'&' '{print $2}' | awk -F'=' '{print $2}')
-file_path="$images_dir/$name"
-curl "$url" -o $file_path || exit 1
-
 template="$HOME/scripts/bg-saved.cfg" 
 config="$HOME/.config/nitrogen/bg-saved.cfg"
+url_preffix="http://www.bing.com"
+
+# actual script
+url_suffix=$(curl --no-progress-meter "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1" | xmllint --xpath "string(//url)" -) || exit 1 && \
+url="$url_preffix$url_suffix" && \
+name=$(echo $url | awk -F'&' '{print $2}' | awk -F'=' '{print $2}') && \
+file_path="$images_dir/$name" && \
+curl "$url" -o $file_path && \
 cp "$template" "$config" && \
 sed -i "s|<==>|$file_path|g" "$config" && \
-nitrogen --restore
+nitrogen --restore && \
+exit 0
+
+# return exitcode if above crashed
+exit 1
 
