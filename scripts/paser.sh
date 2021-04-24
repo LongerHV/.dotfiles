bc21c48f1e0988aebd6f3438530f13e13b5e6709
Michał Mieszczak <michal@mieszczak.com.pl>
2021-04-24 09:43:01 +0200

    Fix paser.sh in dash

--- scripts/paser.sh
+++ scripts/paser.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Interactive sarch and install pacman packages
 
@@ -7,22 +7,19 @@ export FZF_DEFAULT_OPTS='
 --layout=reverse
 --prompt="Package: "'
 
-package=$(pacman -Ss | \
-    awk 'NR % 2 == 1 { print $1 }' | \
-    fzf)
+package=$(pacman -Ssq | fzf)
 
 [ -z "$package" ] && exit 1
 pacman -Si $package | less
-package_short=$(echo $package | awk -F'/' '{print $2}')
 
 echo ":: Selected package: $package"
-pacman -Qq $package_short &> /dev/null && echo ":: Package already in the system"
+pacman -Qq $package &> /dev/null && echo ":: Package already in the system"
 read -p ":: Install package? [y/N] " ans
-if [ "$ans" == "y" ]
+if [ "$ans" = "y" ]
 then
     sudo pacman -S $package
     exit $?
 else
-    exit 1
+    exit 0
 fi
 
