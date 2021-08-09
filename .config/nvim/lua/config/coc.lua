449f930fdb3a023ac7886fc2cda8280b899af09d
LongerHV <michal@mieszczak.com.pl>
2021-08-09 08:57:41 +0200

    Add lua coc extension

--- .config/nvim/lua/config/coc.lua
+++ .config/nvim/lua/config/coc.lua
@@ -1,2 +1,2 @@
-vim.g.coc_global_extensions = {'coc-pyright', 'coc-json', 'coc-yaml', 'coc-vimlsp', 'coc-prettier', 'coc-explorer', 'coc-lists', 'coc-pydocstring', 'coc-markdownlint'}
+vim.g.coc_global_extensions = {'coc-pyright', 'coc-json', 'coc-yaml', 'coc-vimlsp', 'coc-prettier', 'coc-explorer', 'coc-lists', 'coc-pydocstring', 'coc-markdownlint', 'coc-sumneko-lua'}
 vim.api.nvim_set_keymap('n', '<leader>t', ':CocCommand explorer<CR>', {})
