4abe926c428287fcc0c3153674f315aab91f2aee
LongerHV <michal@mieszczak.com.pl>
2021-08-10 12:53:30 +0200

    Update formatting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,57 +15,62 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Plugin specification
-return require('packer').startup(function(use)
-	-- LSP
-	use {'neoclide/coc.nvim', branch = 'release'}
-	use 'rafcamlet/coc-nvim-lua'
-	-- use 'neovim/nvim-lspconfig'
-	-- use 'nvim-lua/completion-nvim'
-	-- use 'nvim-lua/lsp_extensions.nvim'
-	-- use 'nvim-lua/lsp-status.nvim'
-	-- use 'kyazdani42/nvim-tree.lua'
+return require('packer').startup(
+	function(use)
+		-- LSP
+		use {'neoclide/coc.nvim', branch = 'release'}
+		use 'rafcamlet/coc-nvim-lua'
+		-- use 'neovim/nvim-lspconfig'
+		-- use 'nvim-lua/completion-nvim'
+		-- use 'nvim-lua/lsp_extensions.nvim'
+		-- use 'nvim-lua/lsp-status.nvim'
+		-- use 'kyazdani42/nvim-tree.lua'
 
-	-- Syntax highlighting + indents
-	use 'sheerun/vim-polyglot'
+		-- Syntax highlighting + indents
+		use 'sheerun/vim-polyglot'
 
-	-- Treesitter
-	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-	use {
-		'nvim-treesitter/nvim-treesitter-textobjects',
-		requires = 'nvim-treesitter/nvim-treesitter'
-	}
-	use {
-		'nvim-treesitter/playground',
-		requires = 'nvim-treesitter/nvim-treesitter'
-	}
+		-- Treesitter
+		use {
+			'nvim-treesitter/nvim-treesitter',
+			run = ':TSUpdate'
+		}
+		use {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+			requires = 'nvim-treesitter/nvim-treesitter'
+		}
+		use {
+			'nvim-treesitter/playground',
+			requires = 'nvim-treesitter/nvim-treesitter'
+		}
 
-	-- Debugging
-	-- use 'puremourning/vimspector'
-	-- use 'mfussenegger/nvim-dap'
-	-- use 'rcarriga/nvim-dap-ui'
-	-- use 'Pocco81/DAPInstall.nvim'
+		-- Debugging
+		-- use 'puremourning/vimspector'
+		-- use 'mfussenegger/nvim-dap'
+		-- use 'rcarriga/nvim-dap-ui'
+		-- use 'Pocco81/DAPInstall.nvim'
 
-	-- Additional movements
-	use 'blackCauldron7/surround.nvim'
-	use 'tpope/vim-repeat'
-	use 'b3nj5m1n/kommentary'
-	use 'windwp/nvim-autopairs'
+		-- Additional movements
+		use 'blackCauldron7/surround.nvim'
+		use 'tpope/vim-repeat'
+		use 'b3nj5m1n/kommentary'
+		use 'windwp/nvim-autopairs'
 
-	-- Looks
-	use 'glepnir/dashboard-nvim'
-	use 'mhartington/oceanic-next'
-	use 'hoob3rt/lualine.nvim'
-	use 'kyazdani42/nvim-web-devicons'
-	use 'lukas-reineke/indent-blankline.nvim'
-	use 'norcalli/nvim-colorizer.lua'
-	use {
-		'p00f/nvim-ts-rainbow',
-		requires = 'nvim-treesitter/nvim-treesitter'
-	}
+		-- Looks
+		use 'glepnir/dashboard-nvim'
+		use 'mhartington/oceanic-next'
+		use 'hoob3rt/lualine.nvim'
+		use 'kyazdani42/nvim-web-devicons'
+		use 'lukas-reineke/indent-blankline.nvim'
+		use 'norcalli/nvim-colorizer.lua'
+		use {
+			'p00f/nvim-ts-rainbow',
+			requires = 'nvim-treesitter/nvim-treesitter'
+		}
 
-	-- Telescope (Fuzzy finding)
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-	}
-end)
+		-- Telescope (Fuzzy finding)
+		use {
+			'nvim-telescope/telescope.nvim',
+			requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+		}
+	end
+)
