210a623bc5da8d5001a1c5642cde18cc5892ac6f
LongerHV <michal@mieszczak.com.pl>
2021-08-13 10:58:05 +0200

    Use ts node types for indentline context

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -19,7 +19,11 @@ vim.cmd 'hi EndOfBuffer guibg=NONE ctermbg=NONE'
 require'indent_blankline'.setup {
 	use_treesitter = true,
 	show_current_context = true,
-	context_patterns = {'class', 'function', 'method', '^if', '^for', '^while', '^try'},
+	context_patterns = {
+		'class', 'function', 'method', 'for_statement',
+		'try_statement', 'except_clause', 'finally_clause',
+		'while_statement', 'if_statement',
+	},
 	filetype_exclude = {'dashboard', 'coc-explorer'},
 }
 
