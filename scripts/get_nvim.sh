174412a2d879a1f5c254d5473e3f9b9c0bcb03a6
LongerHV <michal@mieszczak.com.pl>
2021-08-30 09:15:01 +0200

    get_nvim script now accepts url as optional parameter

--- scripts/get_nvim.sh
+++ scripts/get_nvim.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-url=https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
+if [ $1 ]
+then
+	url=$1
+else
+	url=https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
+fi
 target=~/Applications/nvim.appimage
 link=~/.local/bin/nvim
 mkdir -p $(dirname ${target})
