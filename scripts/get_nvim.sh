d07d587736b5d7c0a95667247b34f4b44efbec6b
Michał Mieszczak <michal@mieszczak.com.pl>
2021-09-06 22:02:16 +0200

    Chck hash before and after downloading nvim appimage

--- scripts/get_nvim.sh
+++ scripts/get_nvim.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
+
+target_dir=~/Applications
+target=${target_dir}/nvim.appimage
+link=~/.local/bin/nvim
+
 if [ $1 ]
 then
 	url=$1
 else
 	url=https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
 fi
-target=~/Applications/nvim.appimage
-link=~/.local/bin/nvim
+
+cd ${target_dir}
+
 mkdir -p $(dirname ${target})
 mkdir -p $(dirname ${link})
-wget -x -O ${target} ${url} && chmod +x ${target} && ln -sf ${target} ${link}
+
+digest=$(curl -sSL ${url}.sha256sum)
+echo ${digest} | sha256sum --check - > /dev/null
+if [ $? -eq 0 ];
+then
+	echo "Neovim is already up to date"
+	exit 0
+else
+	wget -x -O ${target} ${url} && chmod +x ${target}
+	echo ${digest} | sha256sum --check - || exit
+	ln -sf ${target} ${link}
+fi
