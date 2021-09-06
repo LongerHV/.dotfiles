5d479dbea48215f15b04d6c6804e10d612b13c1d
Michał Mieszczak <michal@mieszczak.com.pl>
2021-09-06 18:41:31 +0200

    Restore old settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,11 +4,6 @@
 vim.g.mapleader = " "
 
 require("config.general")
-if vim.g.vscode == nil then
-	require("plugins")
-	require("config.theme")
-else
-	require("plugins_vscode")
-	vim.o.termguicolors = true
-end
+require("plugins")
+require("config.theme")
 require("config.remaps")
