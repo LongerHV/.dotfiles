0a37716961c8d93416f274d6b354b460fe07ab2c
Michał Mieszczak <michal@mieszczak.com.pl>
2021-09-10 19:22:21 +0200

    Suppress sha output

--- scripts/get_nvim.sh
+++ scripts/get_nvim.sh
@@ -12,13 +12,11 @@ else
 	url=https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage 
 fi
 
-cd ${target_dir}
-
 mkdir -p $(dirname ${target})
 mkdir -p $(dirname ${link})
 
 digest=$(curl -sSL ${url}.sha256sum)
-echo ${digest} | sha256sum --check - > /dev/null
+cd ${target_dir} && echo ${digest} | sha256sum --check - > /dev/null
 if [ $? -eq 0 ];
 then
 	echo "Neovim is already up to date"
