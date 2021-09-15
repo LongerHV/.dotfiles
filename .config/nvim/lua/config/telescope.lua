031228879032db1425aff2b940150cbdb4b78468
LongerHV <michal@mieszczak.com.pl>
2021-09-15 11:21:00 +0200

    Make Telescope wider

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,3 +1,4 @@
+require("telescope").setup({ defaults = { layout_config = { horizontal = { width = 0.9 } } } })
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
