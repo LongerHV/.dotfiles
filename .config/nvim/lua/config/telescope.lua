9f8552ba46f301242bb887f9d290d39f4e1d0656
LongerHV <michal@mieszczak.com.pl>
2021-09-20 12:41:27 +0200

    [Neovim] Set follow for tresitter file browser as well

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,10 +1,17 @@
-require("telescope").setup({ defaults = { layout_config = { horizontal = { width = 0.9 } } } })
+local telescope = require("telescope")
+
+telescope.setup({ defaults = { layout_config = { horizontal = { width = 0.9 } } } })
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>Telescope treesitter<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>Telescope lsp_references<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>Telescope file_browser<cr>", { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fe",
+	"<cmd>lua require('telescope.builtin').file_browser({cwd = require('telescope.utils').buffer_dir()})<cr>",
+	{ noremap = true }
+)
 vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>T", "<cmd>Telescope<cr>", { noremap = true })
