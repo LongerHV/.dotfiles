0d4268524db89dff4c4ec0271eb429916e23b17b
LongerHV <michal@mieszczak.com.pl>
2021-09-20 13:37:39 +0200

    [Neovim] Exit nvim_tree if it is the last buffer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -69,6 +69,7 @@ return require("packer").startup(function(use)
 		config = function()
 			vim.g.nvim_tree_width = 40
 			vim.g.nvim_tree_follow = 1
+			vim.g.nvim_tree_auto_close = 1
 			vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", {})
 		end,
 	})
