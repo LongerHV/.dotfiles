bda26a1a19c2aaf87e9c5d4769f36e6808979c71
Michał Mieszczak <michal@mieszczak.com.pl>
2021-10-07 11:46:57 +0200

    [Neovim] Refactor file_browser picker config

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,17 +1,27 @@
 local telescope = require("telescope")
+local utils = require("telescope.utils")
 
-telescope.setup({ defaults = { layout_config = { horizontal = { width = 0.9 } } } })
+telescope.setup({
+	defaults = {
+		layout_config = {
+			horizontal = {
+				width = 0.9,
+			},
+		},
+	},
+	pickers = {
+		file_browser = {
+			cwd = utils.buffer_dir(),
+		},
+	},
+})
+
 vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>Telescope treesitter<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fr", "<cmd>Telescope lsp_references<cr>", { noremap = true })
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fe",
-	"<cmd>lua require('telescope.builtin').file_browser({cwd = require('telescope.utils').buffer_dir()})<cr>",
-	{ noremap = true }
-)
+vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>Telescope file_browser<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>Telescope commands<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>T", "<cmd>Telescope<cr>", { noremap = true })
